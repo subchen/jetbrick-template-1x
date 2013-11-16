16950b9f557c8d17fd4695f428c336a091a7ff5f
subchen <subchen@gmail.com>
2013-11-16 12:30:07 +0800

    Add LICENSE for source files.

--- src/main/java/jetbrick/template/utils/DateUtils.java
+++ src/main/java/jetbrick/template/utils/DateUtils.java
@@ -1,11 +1,28 @@
+/**
+ * jetbrick-template
+ * http://subchen.github.io/jetbrick-template/
+ *
+ * Copyright 2010-2013 Guoqiang Chen. All rights reserved.
+ * Email: subchen@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jetbrick.template.utils;
 
 import java.text.*;
 import java.util.Date;
 
 public final class DateUtils {
-    private static final FastDateFormat DEFAULT_DATE_FORMAT = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss");
-
     //@formatter:off
     private static final String[] DATE_PATTERNS = new String[] { 
         "yyyy-MM-dd HH:mm:ss.SSS", 
@@ -51,10 +68,10 @@ public final class DateUtils {
     }
 
     public static String format(Date value) {
-        return DEFAULT_DATE_FORMAT.format(value);
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(value);
     }
 
     public static String format(Date value, String format) {
-        return FastDateFormat.getInstance(format).format(value);
+        return new SimpleDateFormat(format).format(value);
     }
 }
