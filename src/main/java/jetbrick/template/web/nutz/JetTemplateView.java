60313ce2a5537257e45f5446d9be66f5e702c69d
subchen <subchen@gmail.com>
2013-11-28 00:06:49 +0800

    Add Nutz.jar for build

--- src/main/java/jetbrick/template/web/nutz/JetTemplateView.java
+++ src/main/java/jetbrick/template/web/nutz/JetTemplateView.java
@@ -1,25 +1,46 @@
+/**
+ * jetbrick-template
+ * http://subchen.github.io/jetbrick-template/
+ *
+ * Copyright 2010-2013 Guoqiang Chen. All rights reserved.
+ * Email: subchen@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jetbrick.template.web.nutz;
 
 import java.io.IOException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import jetbrick.template.JetContext;
 import jetbrick.template.JetTemplate;
 import jetbrick.template.web.JetWebContext;
 import jetbrick.template.web.JetWebEngineLoader;
-
 import org.nutz.lang.Lang;
 import org.nutz.mvc.view.AbstractPathView;
 
+/**
+ * nutz与JetTemplate集成
+ * @author wendal(wendal1985@gmail.com)
+ */
 public class JetTemplateView extends AbstractPathView {
-	
-	public JetTemplateView(String value) {
-		super(value);
-	}
 
-	public void render(HttpServletRequest req, HttpServletResponse resp, Object obj) throws Throwable {
+    public JetTemplateView(String value) {
+        super(value);
+    }
+
+    @Override
+    public void render(HttpServletRequest req, HttpServletResponse resp, Object obj) throws Throwable {
         JetContext context = new JetWebContext(req, resp);
         JetTemplate template = JetWebEngineLoader.getJetEngine().getTemplate(evalPath(req, obj));
         try {
@@ -27,5 +48,5 @@ public class JetTemplateView extends AbstractPathView {
         } catch (IOException e) {
             throw Lang.wrapThrow(e);
         }
-	}
+    }
 }
--- src/main/java/jetbrick/template/web/nutz/JetTemplateViewMaker.java
+++ src/main/java/jetbrick/template/web/nutz/JetTemplateViewMaker.java
@@ -1,11 +1,27 @@
+/**
+ * jetbrick-template
+ * http://subchen.github.io/jetbrick-template/
+ *
+ * Copyright 2010-2013 Guoqiang Chen. All rights reserved.
+ * Email: subchen@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package jetbrick.template.web.nutz;
 
 import jetbrick.template.web.JetWebEngineLoader;
-
 import org.nutz.ioc.Ioc;
-import org.nutz.mvc.Mvcs;
-import org.nutz.mvc.View;
-import org.nutz.mvc.ViewMaker;
+import org.nutz.mvc.*;
 
 /**
  * nutz与JetTemplate集成
@@ -13,15 +29,15 @@ import org.nutz.mvc.ViewMaker;
  */
 public class JetTemplateViewMaker implements ViewMaker {
 
-	public JetTemplateViewMaker() {
-		JetWebEngineLoader.setServletContext(Mvcs.getServletContext());
-	}
-	
-	public View make(Ioc ioc, String type, final String value) {
-		if (JetWebEngineLoader.getTemplateSuffix().equals(type)) {
-			return new JetTemplateView(value);
-		}
-		return null;
-	}
+    public JetTemplateViewMaker() {
+        JetWebEngineLoader.setServletContext(Mvcs.getServletContext());
+    }
+    
+    @Override
+    public View make(Ioc ioc, String type, final String value) {
+        if (JetWebEngineLoader.getTemplateSuffix().equals(type)) {
+            return new JetTemplateView(value);
+        }
+        return null;
+    }
 }
-
