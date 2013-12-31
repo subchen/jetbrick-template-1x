16c02fdc684a8e9bf1b49be556398d8e6d0da54f
subchen <subchen@gmail.com>
2013-12-31 10:28:08 +0800

    Fixed #38 Add default cache tag.

--- src/main/java/jetbrick/template/runtime/JetTags.java
+++ src/main/java/jetbrick/template/runtime/JetTags.java
@@ -21,8 +21,12 @@ package jetbrick.template.runtime;
 
 import java.io.IOException;
 import java.util.Map;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
 import jetbrick.template.JetContext;
 import jetbrick.template.JetTemplate;
+import jetbrick.template.utils.TimedSizeCache;
+import jetbrick.template.web.JetWebContext;
 
 /**
  * 系统自带的 Tag
@@ -31,10 +35,10 @@ public final class JetTags {
 
     /**
      * 实现 layout 的 tag, 将内容插入到 ${bodyContent}
-     * 
+     *
      * @param ctx Tag 上下文对象
      * @param file layout 模板路径
-     * 
+     *
      * @since 1.1.0
      */
     public static void layout(JetTagContext ctx, String file) {
@@ -43,10 +47,10 @@ public final class JetTags {
 
     /**
      * 实现 layout 的 tag, 将内容插入到 ${bodyContent}，支持传递参数
-     * 
+     *
      * @param ctx Tag 上下文对象
      * @param file layout 模板路径
-     * 
+     *
      * @since 1.1.1
      */
     public static void layout(JetTagContext ctx, String file, Map<String, Object> parameters) {
@@ -66,10 +70,10 @@ public final class JetTags {
 
     /**
      * 将一个 Block 的内容保存到一个 JetContext 变量中。
-     * 
+     *
      * @param ctx Tag 上下文对象
      * @param name 保存到 JetContext 的变量名
-     * 
+     *
      * @since 1.1.1
      */
     public static void block(JetTagContext ctx, String name) {
@@ -79,10 +83,10 @@ public final class JetTags {
 
     /**
      * 如果不存在指定的 JetContext 变量，那么输出 default_block 块内容，否则输出指定的 JetContext 变量。
-     * 
+     *
      * @param ctx Tag 上下文对象
      * @param name JetContext 的变量名
-     * 
+     *
      * @since 1.1.2
      */
     public static void default_block(JetTagContext ctx, String name) throws IOException {
@@ -92,5 +96,53 @@ public final class JetTags {
         } else {
             ctx.getWriter().print(value);
         }
+    }
+
+    /**
+     * ServletContext 级别的 cache.
+     *
+     * @param ctx Tag 上下文对象
+     * @param name cache 名称
+     * @param timeout 超时时间，单位秒
+     *
+     * @since 1.2.0
+     */
+    public static void application_cache(JetTagContext ctx, String name, long timeout) throws IOException {
+        ServletContext sc = (ServletContext) ctx.getContext().get(JetWebContext.SERVLET_CONTEXT);
+        TimedSizeCache cache = (TimedSizeCache) sc.getAttribute(TimedSizeCache.CACHE_KEY);
+        if (cache == null) {
+            cache = new TimedSizeCache(128);
+            sc.setAttribute(TimedSizeCache.CACHE_KEY, cache);
+        }
+        Object value = cache.get(name);
+        if (value == null) {
+            value = ctx.getBodyContent();
+            cache.put(name, value, timeout * 1000);
+        }
+        ctx.getWriter().print(value);
+    }
+
+    /**
+     * HttpSession 级别的 cache.
+     *
+     * @param ctx Tag 上下文对象
+     * @param name cache 名称
+     * @param timeout 超时时间，单位秒
+     *
+     * @since 1.2.0
+     */
+    public static void session_cache(JetTagContext ctx, String name, long timeout) throws IOException {
+        HttpSession session = (HttpSession) ctx.getContext().get(JetWebContext.SESSION);
+        TimedSizeCache cache = (TimedSizeCache) session.getAttribute(TimedSizeCache.CACHE_KEY);
+        if (cache == null) {
+            cache = new TimedSizeCache(128);
+            session.setAttribute(TimedSizeCache.CACHE_KEY, cache);
+        }
+        Object value = cache.get(name);
+        if (value == null) {
+            value = ctx.getBodyContent();
+            cache.put(name, value, timeout * 1000);
+        }
+        ctx.getWriter().print(value);
     }
 }
---
+++ src/main/java/jetbrick/template/utils/TimedSizeCache.java
@@ -0,0 +1,158 @@
+/**
+ * jetbrick-template
+ * http://subchen.github.io/jetbrick-template/
+ *
+ * Copyright 2010-2013 Guoqiang Chen. All rights reserved.
+ * Email: subchen@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jetbrick.template.utils;
+
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 简单具有超时检测的 cache 实现.
+ *
+ * @since 1.20.
+ * @author Guoqiang Chen
+ */
+public class TimedSizeCache {
+    public static final String CACHE_KEY = TimedSizeCache.class.getName();
+
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
+    protected Map<String, CacheEntry> cacheMap;
+    protected int maxSize; // max cache size, 0 = no limit
+
+    public TimedSizeCache(int maxSize) {
+        this.maxSize = maxSize;
+        this.cacheMap = new HashMap<String, CacheEntry>();
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void put(String key, Object object) {
+        put(key, object, 0);
+    }
+
+    public void put(String key, Object object, long timeout) {
+        writeLock.lock();
+        try {
+            CacheEntry entry = new CacheEntry(key, object, timeout);
+            if (isFull()) {
+                pruneCache();
+            }
+            cacheMap.put(key, entry);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public Object get(String key) {
+        readLock.lock();
+        try {
+            CacheEntry entry = cacheMap.get(key);
+            if (entry == null) {
+                return null;
+            }
+            if (entry.isExpired()) {
+                cacheMap.remove(key);
+                return null;
+            }
+            return entry.getObject();
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public void prune() {
+        writeLock.lock();
+        try {
+            pruneCache();
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean isFull() {
+        if (maxSize == 0) {
+            return false;
+        }
+        return cacheMap.size() >= maxSize;
+    }
+
+    public void remove(String key) {
+        writeLock.lock();
+        try {
+            cacheMap.remove(key);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public void clear() {
+        writeLock.lock();
+        try {
+            cacheMap.clear();
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    public int size() {
+        return cacheMap.size();
+    }
+
+    private void pruneCache() {
+        Iterator<CacheEntry> values = cacheMap.values().iterator();
+        while (values.hasNext()) {
+            CacheEntry entry = values.next();
+            if (entry.isExpired()) {
+                values.remove();
+            }
+        }
+    }
+
+    static class CacheEntry {
+        final String key;
+        final Object cachedObject;
+        long expiredTime; // time of expire
+
+        CacheEntry(String key, Object object, long ttl) {
+            this.key = key;
+            this.cachedObject = object;
+            this.expiredTime = (ttl == 0) ? 0 : System.currentTimeMillis() + ttl;
+        }
+
+        boolean isExpired() {
+            if (expiredTime == 0) {
+                return false;
+            }
+            return expiredTime < System.currentTimeMillis();
+        }
+
+        Object getObject() {
+            return cachedObject;
+        }
+    }
+}
